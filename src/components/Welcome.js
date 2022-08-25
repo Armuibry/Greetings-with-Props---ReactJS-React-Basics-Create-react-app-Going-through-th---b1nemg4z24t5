c104803846549fe9713c81dd2ef3b5601a65409f
Md Amir Sohail <mdamirsohail1@gmail.com>
2022-08-25 13:50:12 +0530

    greeting

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,8 +3,7 @@ import React from "react";
 function Welcome(props){
     return(
         <>
-            Hey !
-            <h1>{props.name}</h1>
+            <h1>{props.greeting}{props.name}</h1>
             <h2>Welcome to Newton School</h2>
         </>
     )
